e4c9d7d48527f1e3a664d2158d2e8fc891d0e564
aliyu <1324149139@qq.com>
2022-04-20 16:58:33 +0800

    deploy

---
+++ precache-manifest.dade99427d785abe76c51b4d01ee95ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9813f491ebda9016ef7",
+    "url": "/mormoney-website/css/app.94692f79.css"
+  },
+  {
+    "revision": "059001edf855b676d8838fcce1751147",
+    "url": "/mormoney-website/index.html"
+  },
+  {
+    "revision": "c9813f491ebda9016ef7",
+    "url": "/mormoney-website/js/app.1dac1ec7.js"
+  },
+  {
+    "revision": "66861be0c64db5ab62f4",
+    "url": "/mormoney-website/js/chunk-vendors.eae1850c.js"
+  },
+  {
+    "revision": "124e2d289a2fe5170ac3544f0e65b5ce",
+    "url": "/mormoney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mormoney-website/robots.txt"
+  }
+]);
